bcc5890157ea3eede5941cc003cea5035996dc5a
NjB <njbertha6@gmail.com>
2023-03-21 15:32:00 +0100

    nginx modif

--- prom_stack/install-nginx.sh
+++ prom_stack/install-nginx.sh
@@ -1,9 +1,9 @@
 #Nginx Installation
 
 sudo apt install nginx
-sudo service nginx status
 
 sudo cp grafana.my-coolpay.com.conf /etc/nginx/sites-enabled/
 
 sudo systemctl enable nginx
 sudo systemctl start nginx
+sudo systemctl status nginx
