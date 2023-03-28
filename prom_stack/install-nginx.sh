85e737e4d4fda73f1c5b0b409f32c0061cadb31e
NjB <njbertha6@gmail.com>
2023-03-28 16:19:50 +0100

    ajouts

--- prom_stack/install-nginx.sh
+++ prom_stack/install-nginx.sh
@@ -6,6 +6,11 @@ sudo cp grafana.my-coolpay.com.conf /etc/nginx/sites-enabled/
 sudo cp prometeus.my-coolpay.com.conf /etc/nginx/sites-enabled/
 sudo cp alertmanager.my-coolpay.com.conf /etc/nginx/sites-enabled/
 
+sudo apt-get install apache2-utils
+cd /etc/prometheus
+sudo htpasswd -c .credentials admin 
+
+
 sudo systemctl enable nginx
 sudo systemctl start nginx
 sudo systemctl status nginx
---
+++ prom_stack/lets-encrypt.sh
@@ -0,0 +1,8 @@
+#Cerbot Installation for LetsEncrypt SSL certificates
+#!/bin/bash
+
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+sudo certbot --nginx
